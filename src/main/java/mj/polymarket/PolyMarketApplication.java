c2a20f378b2206a2d6ccc18bf05bb21ef71b9f3d
jung min jun <alswns7984@naver.com>
2022-03-30 18:31:58 +0900

    프로젝트 초기화

---
+++ src/main/java/mj/polymarket/PolyMarketApplication.java
@@ -0,0 +1,13 @@
+package mj.polymarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolyMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PolyMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mj/polymarket/PolyMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package mj.polymarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolyMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
