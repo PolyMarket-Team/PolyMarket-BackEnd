e0ac30e849311e7e41e5a4c390e21f6f0436f36f
jung min jun <alswns7984@naver.com>
2022-05-11 21:28:40 +0900

    토큰 재발급 성공 응답메시지 작성

--- src/main/java/kr/polymarket/global/result/ResultCode.java
+++ src/main/java/kr/polymarket/global/result/ResultCode.java
@@ -9,7 +9,8 @@ public enum ResultCode {
     EMAIL_SEND_SUCCESS(200,"M000", "이메일전송에 성공하였습니다"),
     SIGNUP_SUCCESS(200, "M001", "회원가입 되었습니다."),
     SIGNIN_SUCCESS(200, "m002", "로그인에 성공하였습니다."),
-    CONFIRM_EMAIL_SUCCESS(200, "M04", "이메일 인증에 성공하셨습니다.");
+    CONFIRM_EMAIL_SUCCESS(200, "M003", "이메일 인증에 성공하셨습니다."),
+    REFRESH_SUCCESS(200, "M004", "토큰 재발급에 성공하셨습니다.");
 
     private int status;
     private final String code;
