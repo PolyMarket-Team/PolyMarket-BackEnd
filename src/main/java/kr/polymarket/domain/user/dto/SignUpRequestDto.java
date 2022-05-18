fbcb1919791a26a06cb22e96dce7af988452794b
Shin Woo <sinwoo1225@gmail.com>
2022-05-18 19:10:31 +0900

    로그인 , 회원가입, 회원 프로필 수정 요청값 검증 추가

--- src/main/java/kr/polymarket/domain/user/dto/SignUpRequestDto.java
+++ src/main/java/kr/polymarket/domain/user/dto/SignUpRequestDto.java
@@ -24,10 +24,12 @@ public class SignUpRequestDto {
     @Pattern(regexp ="^(?=.*[A-Za-z])(?=.*\\d)(?=.*[~!@#$%^&*()+|=])[A-Za-z\\d~!@#$%^&*()+|=]{8,20}$",
             message = "비밀번호는 특수문자, 영어, 숫자가 모두 포함되어야합니다.")
     @Length(min = 8, max = 20, message = "비밀번호는 8자 이상 20자 이하여야 합니다.")
+    @NotBlank(message = "패스워드를 입력해주세요.")
     @ApiModelProperty(name = "패스워드", notes = "패스워드(특수문자, 영어, 숫자가 모두 포함)", required = true, example = "q1w2e3r4!@#")
     private String password;
 
     @Length(min = 2, max = 10, message = "닉네임은 2자 이상 10자 이하여야 합니다.")
+    @NotBlank(message = "별명을 입력해주세요")
     @ApiModelProperty(name = "별명", notes = "별명(2~10자)", required = true, example = "별명")
     private String nickname;
 
--- src/main/java/kr/polymarket/domain/user/dto/UserProfileUpdateRequestDto.java
+++ src/main/java/kr/polymarket/domain/user/dto/UserProfileUpdateRequestDto.java
@@ -3,6 +3,11 @@ package kr.polymarket.domain.user.dto;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -12,9 +17,13 @@ import lombok.*;
 public class UserProfileUpdateRequestDto {
 
     @ApiModelProperty(name = "사용자 별명", notes = "사용자 별명", example = "별명")
+    @Length(min = 2, max = 10, message = "닉네임은 2자 이상 10자 이하여야 합니다.")
+    @NotBlank(message = "별명을 입력해주세요")
     private String nickname;
 
     @ApiModelProperty(name = "사용자 ID", notes = "사용자 ID", example = "99999")
+    @NotNull(message = "프로필 이미지 파일아이디를 반드시 입력해야합니다.")
+    @Range(min = 1, message = "파일아이디는 반드시 1이상이어야합니다.")
     private Long profileImageFileId;
 
 }
